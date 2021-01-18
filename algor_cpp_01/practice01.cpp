1f3bcaec51392074001865b79afbdebfc236af4a
hjjja <jahj30734@gmail.com>
2021-01-18 14:32:34 +0900

    majority

---
+++ algor_cpp_01/practice01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int majority2(const vector<int>& A)
+{
+	int N = A.size();
+	vector<int> count(101, 0);
+	for (int i = 0; i < N; ++i) {
+		count[A[i]]++;
+	}
+	int majority = 0;
+	for (int i = 1; i <= 100; ++i) {
+		if (count[i] > count[majority]) {
+			majority = i;
+		}
+	}
+	return majority;
+}
+
+
+int main() 
+{
+	const vector<int> A = {27, 90, 90,90, 60, 70, 0, 40, 40, 37};
+
+	cout << majority2(A);
+	
+	return 0;
+}
